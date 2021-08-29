6b452b1153ecbdc12767c2749cc1e57a4ef32ab8
GH0STsama <leonardoduran2004@gmail.com>
2021-08-29 18:54:47 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 from telegram.ext import Updater, MessageHandler, Filters
 from os import getenv
 
-
 BOT_CHAT_ID = getenv("BOT_CHAT_ID")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
@@ -13,6 +12,9 @@ def mensaje(update, context):
     if update.effective_user.id in users_perm and str(msg).__contains__(s):
         context.bot.delete_message(message_id = update.message.message_id, chat_id = update.message.chat_id)
         context.bot.send_message(chat_id = BOT_CHAT_ID, text = str(msg).replace(s, ""))
+    elif update.effective_user.id in users_perm and str(msg).__contains__("#s3"):
+        context.bot.send_message(chat_id = "@yameteselo", text = str(msg).replace("#s3", ""))
+
 
 updater = Updater(token = BOT_TOKEN, use_context = True)
 dp = updater.dispatcher
