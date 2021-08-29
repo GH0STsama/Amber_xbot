67744383b2ce2285dd192edd03b371e279252529
GH0STsama <leonardoduran2004@gmail.com>
2021-08-29 19:15:31 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,9 +14,7 @@ def mensaje(update, context):
         context.bot.delete_message(message_id = update.message.message_id, chat_id = update.message.chat_id)
         context.bot.send_message(chat_id = BOT_CHAT_ID, text = str(msg).replace(s, ""))
     elif update.effective_user.id in users_perm and str(msg).__contains__("#s3"):
-        
         context.bot.send_message(chat_id = "-1001588957727", text = str(msg).replace("#s3", ""))
-
 
 updater = Updater(token = BOT_TOKEN, use_context = True)
 dp = updater.dispatcher
