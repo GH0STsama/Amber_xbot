c413eb504e5f800e995cb2ad7eb99a3c1a14f028
GH0STsama <leonardoduran2004@gmail.com>
2021-08-31 12:10:02 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,13 +12,20 @@ def mensaje(update, context):
         context.bot.delete_message(message_id = update.message.message_id, chat_id = update.message.chat_id)
         context.bot.send_message(chat_id = BOT_CHAT_ID, text = str(msg).replace("/xd", ""))
     elif update.effective_user.id in users_perm and str(msg).__contains__("#s3"):
-        context.bot.send_message(chat_id = "-1001402527951", text = str(msg).replace("#s3", ""))
+        context.bot.send_message(chat_id = "@GGcompanyS3", text = str(msg).replace("#s3", ""))
     else:
         pass
+
+def photo(update, context):
+    if not update.effective_user['first_name'] == 'Telegram':
+        caption = update.message.caption
+        if update.effective_user.id in users_perm and str(caption).__contains__("#s3"):
+            context.bot.send_photo(chat_id= "@canaldepruebaxdasdtumama", photo = update.message.message_id,caption = caption)
 
 updater = Updater(token = BOT_TOKEN, use_context = True)
 dp = updater.dispatcher
 dp.add_handler(MessageHandler(Filters.text, callback = mensaje))
+dp.add_handler(MessageHandler(Filters.photo, callback = photo))
 
 updater.start_polling()
 print("Bot Iniciado!")
