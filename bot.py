6f49100c40c38c83b12130d6d8ef9e58fe6756a6
GH0STsama <leonardoduran2004@gmail.com>
2021-09-01 14:27:31 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,27 +5,35 @@ BOT_CHAT_ID = getenv("BOT_CHAT_ID")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 users_perm = [1816693475, 1325010317]
+canal = "@GGcompanyS3"
 
 def mensaje(update, context):
     msg = update.message.text
     if update.effective_user.id in users_perm and str(msg).__contains__("/xd"):
         context.bot.delete_message(message_id = update.message.message_id, chat_id = update.message.chat_id)
         context.bot.send_message(chat_id = BOT_CHAT_ID, text = str(msg).replace("/xd", ""))
-    elif update.effective_user.id in users_perm and str(msg).__contains__("#s3"):
-        context.bot.send_message(chat_id = "@GGcompanyS3", text = str(msg).replace("#s3", ""))
-    else:
-        pass
+    if update.effective_user.id in users_perm and str(msg).__contains__("#s3"):
+        context.bot.send_message(chat_id = canal, text = str(msg).replace("#s3", ""))
 
-def photo(update, context):
-    if not update.effective_user['first_name'] == 'Telegram':
-        caption = update.message.caption
-        if update.effective_user.id in users_perm and str(caption).__contains__("#s3"):
-            context.bot.send_photo(chat_id= "@canaldepruebaxdasdtumama", photo = update.message.message_id,caption = caption)
+def fotos(update, context):
+    imagen = update.message.photo[0].file_id
+    comentario = update.message.caption
+    if update.effective_user.id in users_perm and str(comentario).__contains__("#s3"):
+        comentario = str(comentario).replace("#s3", "")
+        context.bot.send_photo(photo = imagen, chat_id = canal, caption = comentario)
+
+def documento(update, context):
+    documento = update.message.document.file_id
+    comentario = update.message.caption
+    if update.effective_user.id in users_perm and str(comentario).__contains__("#s3"):
+        comentario = str(comentario).replace("#s3", "")
+        context.bot.send_document(chat_id = canal, document = documento, caption = comentario)
 
 updater = Updater(token = BOT_TOKEN, use_context = True)
 dp = updater.dispatcher
-dp.add_handler(MessageHandler(Filters.text, callback = mensaje))
-dp.add_handler(MessageHandler(Filters.photo, callback = photo))
+dp.add_handler(MessageHandler(Filters.text, mensaje))
+dp.add_handler(MessageHandler(Filters.photo, fotos))
+dp.add_handler(MessageHandler(Filters.document, documento))
 
 updater.start_polling()
 print("Bot Iniciado!")
