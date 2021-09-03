70ab72ddc0f0b5f78f5a7f89dd2ba0ea40ba2206
GH0STsama <leonardoduran2004@gmail.com>
2021-09-03 11:29:47 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from os import getenv
 
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = "1846073090:AAFV2HQxwySo2OtkZcjcoPAdBjs59x9rB5U"
 
 users_perm = [1816693475, 1325010317]
 canal = "@GGcompanyS3"
@@ -33,18 +33,18 @@ def documento(update, context):
         context.bot.send_document(chat_id = canal, document = documento, caption = comentario)
 
 def promo(update, context):
-    canal = InlineKeyboardButton("CANAL", url = "https://t.me/GGcompanyS3")
+    canal = InlineKeyboardButton("⛩CANAL⛩", url = "https://t.me/GGcompanyS3")
     grupo = InlineKeyboardButton("💬CHAT💬", url = "https://t.me/joinchatqM9TvYSDdxmMDUx")
     pxp1 = InlineKeyboardButton("📣PXP📣", url = "http://t.me/Kaneki59")
-    pxp2 = InlineKeyboardButton("PXP", url = "http://t.me/D10S3GEEK")
-    update.message.reply_text(text = "https://t.me/GGcompanyS3", reply_markup = InlineKeyboardMarkup([canal, grupo], [pxp1], [pxp2]))
+    pxp2 = InlineKeyboardButton("📢PXP📢", url = "http://t.me/D10S3GEEK")
+    update.message.reply_text(text = "https://t.me/GGcompanyS3", reply_markup = InlineKeyboardMarkup([[canal, grupo],[pxp1],[pxp2]]))
 
 updater = Updater(token = BOT_TOKEN, use_context = True)
 dp = updater.dispatcher
+dp.add_handler(CommandHandler("promo", promo))
 dp.add_handler(MessageHandler(Filters.text, mensaje))
 dp.add_handler(MessageHandler(Filters.photo, fotos))
 dp.add_handler(MessageHandler(Filters.document, documento))
-dp.add_handler(CommandHandler("promo", promo))
 
 updater.start_polling()
 print("Bot Iniciado!")
