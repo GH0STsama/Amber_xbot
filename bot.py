a992c239ea6408a2640329c55b82a77d0b384ba3
GH0STsama <leonardoduran2004@gmail.com>
2021-09-03 13:15:39 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,46 +5,56 @@ from os import getenv
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 users_perm = [1816693475, 1325010317]
-canal = "@GGcompanyS3"
-grupo = "-1001190361827"
-enviar_grupo = "/xd"
-enviar_canal = "#s3"
+channel_id = "@GGcompanyS3"
+group_id = "-1001190361827"
+for_group = "/xd"
+for_channel = "#s3"
 
-def mensaje(update, context):
+def messages(update, context):
     msg = update.message.text
-    if update.effective_user.id in users_perm and str(msg).__contains__(enviar_grupo):
+    if update.effective_user.id in users_perm and str(msg).__contains__(for_group):
         context.bot.delete_message(message_id = update.message.message_id, chat_id = update.message.chat_id)
-        context.bot.send_message(chat_id = grupo, text = str(msg).replace(enviar_grupo, ""))
-    if update.effective_user.id in users_perm and str(msg).__contains__(enviar_canal):
-        context.bot.send_message(chat_id = canal, text = str(msg).replace(enviar_canal, ""))
+        context.bot.send_message(chat_id = group_id, text = str(msg).replace(for_group, ""))
+    if update.effective_user.id in users_perm and str(msg).__contains__(for_channel):
+        context.bot.send_message(chat_id = channel_id, text = str(msg).replace(for_channel, ""))
 
-def fotos(update, context):
+def photo_to_channel(update, context):
     imagen = update.message.photo[0].file_id
     comentario = update.message.caption
-    if update.effective_user.id in users_perm and str(comentario).__contains__(enviar_canal):
-        comentario = str(comentario).replace(enviar_canal, "")
-        context.bot.send_photo(photo = imagen, chat_id = canal, caption = comentario)
+    if update.effective_user.id in users_perm and str(comentario).__contains__(for_channel):
+        comentario = str(comentario).replace(for_channel, "")
+        context.bot.send_photo(photo = imagen, chat_id = channel_id, caption = comentario)
 
-def documento(update, context):
+def document_to_channel(update, context):
     documento = update.message.document.file_id
     comentario = update.message.caption
-    if update.effective_user.id in users_perm and str(comentario).__contains__(enviar_canal):
-        comentario = str(comentario).replace(enviar_canal, "")
-        context.bot.send_document(chat_id = canal, document = documento, caption = comentario)
+    if update.effective_user.id in users_perm and str(comentario).__contains__(for_channel):
+        comentario = str(comentario).replace(for_channel, "")
+        context.bot.send_document(chat_id = channel_id, document = documento, caption = comentario)
+
+button_channel_id = InlineKeyboardButton("⛩channel_id⛩", url = "https://t.me/GGcompanyS3")
+button_group_id = InlineKeyboardButton("💬CHAT💬", url = "https://t.me/joinchatqM9TvYSDdxmMDUx")
+button_pxp1 = InlineKeyboardButton("📣PXP📣", url = "http://t.me/Kaneki59")
+button_pxp2 = InlineKeyboardButton("📢PXP📢", url = "http://t.me/D10S3GEEK")
+
+def send_channel(update, context):
+    context.bot.send_message(chat_id = channel_id, text = "https://t.me/GGcompanyS3", reply_markup = InlineKeyboardMarkup([[button_channel_id, button_group_id],[button_pxp1],[button_pxp2]]))
+
+def send_user(update, context):
+    context.bot.send_message(chat_id = update.effective_user.id, text = "https://t.me/GGcompanyS3", reply_markup = InlineKeyboardMarkup([[button_channel_id, button_group_id],[button_pxp1],[button_pxp2]]))
 
 def promo(update, context):
-    button_canal = InlineKeyboardButton("⛩CANAL⛩", url = "https://t.me/GGcompanyS3")
-    button_grupo = InlineKeyboardButton("💬CHAT💬", url = "https://t.me/joinchatqM9TvYSDdxmMDUx")
-    button_pxp1 = InlineKeyboardButton("📣PXP📣", url = "http://t.me/Kaneki59")
-    button_pxp2 = InlineKeyboardButton("📢PXP📢", url = "http://t.me/D10S3GEEK")
-    update.message.reply_text(text = "https://t.me/GGcompanyS3", reply_markup = InlineKeyboardMarkup([[button_canal, button_grupo],[button_pxp1],[button_pxp2]]))
+    button1 = InlineKeyboardButton("👤 A mi", send_user)
+    button2 = InlineKeyboardButton("📢 Al channel_id", send_channel)
+    if update.effective_user.id in users_perm:
+        update.message.reply_text("A donde desea enviar el mensaje?", reply_markup = InlineKeyboardMarkup([[button1][button2]]))
 
 updater = Updater(token = BOT_TOKEN, use_context = True)
 dp = updater.dispatcher
 dp.add_handler(CommandHandler("promo", promo))
-dp.add_handler(MessageHandler(Filters.text, mensaje))
-dp.add_handler(MessageHandler(Filters.photo, fotos))
-dp.add_handler(MessageHandler(Filters.document, documento))
+dp.add_handler(MessageHandler(Filters.text, messages))
+dp.add_handler(MessageHandler(Filters.photo, photo_to_channel))
+dp.add_handler(MessageHandler(Filters.document, document_to_channel))
 
 updater.start_polling()
 print("Bot Iniciado!")
