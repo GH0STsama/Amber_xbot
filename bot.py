e5af60a7c3bde9f153d9dcee124643a689f9ad8e
GH0STsama <leonardoduran2004@gmail.com>
2021-09-10 14:51:13 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from os import getenv
 
 BOT_TOKEN = getenv("BOT_TOKEN")
+users_perm = getenv("USERS_PERM")
 
-users_perm = [1816693475, 1325010317]
 channel_id = "@GGcompanyS3"
 group_id = "-1001190361827"
 for_group = "/xd"
